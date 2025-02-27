6d351df285edce17388e05c1cbc92d355123ed1a
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2025-02-27 23:11:46 +0100

    add: configurable cahe props

--- src/main/java/htt/autocache/CacheAutoConfiguration.java
+++ src/main/java/htt/autocache/CacheAutoConfiguration.java
@@ -2,6 +2,7 @@ package htt.autocache;
 
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
@@ -11,6 +12,7 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -18,11 +20,21 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name = "cachelib.enabled", havingValue = "true", matchIfMissing = true)
 public class CacheAutoConfiguration {
 
+
+    @Value("${cachelib.max-size:1000}")
+    private int maxSize;
+
+    @Value("${cachelib.expire-after-write:10}")
+    private int expireAfterWrite;
+
     @Bean
     @ConditionalOnMissingBean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(Caffeine.newBuilder().maximumSize(1000));
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireAfterWrite, TimeUnit.MINUTES));
         return cacheManager;
     }
+
 }
